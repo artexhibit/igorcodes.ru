b488caaabe63f77f06d63ae0aba6dfd185535f71
artexhibit <vendabal@yandex.ru>
2023-08-04 23:28:20 +0300

    add a script to change header social icon on hover, setup media mixins

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 //change header social icon color on hover
 let socialItems = document.querySelectorAll(".socials__item");
+let isHovered = false;
 
 socialItems.forEach((item) => {
     item.addEventListener("mouseover", itemHoveredIn);
-    item.addEventListener("mouseout", itemHoveredOut);
+    item.addEventListener("mouseleave", itemHoveredOut);
 });
 
 function itemHoveredIn(event) {
-    if (event.target.tagName == "LI") {
-        console.log(event.target);
+    if (event.target.tagName == "LI" && !isHovered) {
+        let image = event.target.querySelector("img");
+
+        if (image.alt == "stackOverflow") {
+            image.src = "./assets/images/header/stackOverflowHover.svg";
+        } else if (image.alt == "telegram") {
+            image.src = "./assets/images/header/telegramHover.svg";
+        } else if (image.alt == "envelope") {
+            image.src = "./assets/images/header/envelopeHover.svg";
+        }
+        isHovered = true;
     }
 }
 
-function itemHoveredOut(event) {}
+function itemHoveredOut(event) {
+    let image = event.target.querySelector("img");
+
+        if (image.alt == "stackOverflow") {
+            image.src = "./assets/images/header/stackOverflowDark.svg";
+        } else if (image.alt == "telegram") {
+            image.src = "./assets/images/header/telegramDark.svg";
+        } else if (image.alt == "envelope") {
+            image.src = "./assets/images/header/envelopeDark.svg";
+        }
+    isHovered = false;
+}

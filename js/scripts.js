19af226ec9b2a9c0bac9847bdabcf0547aa17a54
artexhibit <vendabal@yandex.ru>
2023-08-06 23:39:24 +0300

    add esm screen size, complete scss for hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,46 @@ function itemHoveredIn(event) {
 function itemHoveredOut(event) {
     let image = event.target.querySelector("img");
 
-        if (image.alt == "stackOverflow") {
-            image.src = "./assets/images/header/stackOverflowDark.svg";
-        } else if (image.alt == "telegram") {
-            image.src = "./assets/images/header/telegramDark.svg";
-        } else if (image.alt == "envelope") {
-            image.src = "./assets/images/header/envelopeDark.svg";
-        }
+    if (image.alt == "stackOverflow") {
+        image.src = "./assets/images/header/stackOverflowDark.svg";
+    } else if (image.alt == "telegram") {
+        image.src = "./assets/images/header/telegramDark.svg";
+    } else if (image.alt == "envelope") {
+        image.src = "./assets/images/header/envelopeDark.svg";
+    }
     isHovered = false;
+}
+
+//show menu and animate hamburger icon
+let hamburger = document.querySelector(".hamburger");
+let hamburgerTitle = document.querySelector(".hamburger__title");
+let hamburgerBarsContainer = document.querySelector(".hamburger__bars-container");
+let navMenu = document.querySelector(".header__nav-container");
+let menuLinks = document.querySelectorAll(".menu__link");
+
+hamburger.addEventListener("click", showMenu);
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+});
+
+function changeHamburgerTitle() {
+    hamburgerTitle.classList.toggle("fade-out");
+
+    setTimeout(() => {
+        hamburgerTitle.textContent = navMenu.classList.contains("active") ? "Закрыть" : "Меню";
+        hamburgerTitle.classList.toggle("fade-out");
+    }, 250);
+}
+
+function showMenu() {
+    hamburgerBarsContainer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    changeHamburgerTitle();
+}
+
+function closeMenu() {
+    hamburgerBarsContainer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    changeHamburgerTitle();
 }

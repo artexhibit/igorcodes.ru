a7ae90177ea72c6d339fa0209084588eab4260f2
artexhibit <vendabal@yandex.ru>
2023-08-14 23:51:08 +0300

    add blur to header, fix for container in name.js, start to applu css to projects section

--- js/name.js
+++ js/name.js
@@ -1,27 +1,27 @@
 //make equal width if switch on portrait orientation
-let containers = [...document.querySelectorAll(".container")].slice(1);
-
+let container = [...document.querySelectorAll(".container")][1];
 window.addEventListener("resize", handleOrientation);
 
 function handleOrientation() {
     let windowNewHeight = window.innerHeight;
 
-    containers.forEach((container) => {
-        let wrapper = container.querySelector(".wrapper");
-        let image = container.querySelector(".name__image");
-        let title = container.querySelector(".name__title");
-        let subtitle = container.querySelector(".name__subtitle");
+    let section = document.querySelector(".name");
+    let wrapper = container.querySelector(".wrapper");
+    let image = container.querySelector(".name__image");
+    let title = container.querySelector(".name__title");
+    let subtitle = container.querySelector(".name__subtitle");
 
-        if (windowNewHeight <= 568) {
-            wrapper.style.maxWidth = 400 + "px";
-            image.style.width = 218.5 + "px";
-            title.style.fontSize = 1.93 + "rem";
-            subtitle.style.fontSize = 1.16 + "rem";
-        } else {
-            wrapper.style.maxWidth = "";
-            image.style.width = "";
-            title.style.fontSize = "";
-            subtitle.style.fontSize = "";
-        }
-    });
+    if (windowNewHeight <= 536) {
+        wrapper.style.maxWidth = 400 + "px";
+        image.style.width = 218.5 + "px";
+        title.style.fontSize = 1.93 + "rem";
+        subtitle.style.fontSize = 1.16 + "rem";
+        section.style.height = 'calc(100svh + 89px)';
+    } else {
+        wrapper.style.maxWidth = "";
+        image.style.width = "";
+        title.style.fontSize = "";
+        subtitle.style.fontSize = "";
+        section.style.height = 'calc(100svh - 89px)';
+    }
 }
